304973cd42728e6dca5097c8d10b96f56fefceb1
enessancar <enes57751907@gmail.com>
2024-02-06 22:34:57 +0300

    font networdk added

---
+++ Proje/Font.swift
@@ -0,0 +1,16 @@
+//
+//  Font.swift
+//  Proje
+//
+//  Created by Enes Sancar on 6.02.2024.
+//
+
+import Foundation
+
+struct Font {
+    
+    private init() {}
+    
+    static let headline: CGFloat = 20
+    static let body: CGFloat = 16
+}
---
+++ Proje/Network.swift
@@ -0,0 +1,14 @@
+//
+//  Network.swift
+//  Proje
+//
+//  Created by Enes Sancar on 6.02.2024.
+//
+
+import Foundation
+
+final class Network {
+    
+    static let shared = Network()
+    private init () {}
+}

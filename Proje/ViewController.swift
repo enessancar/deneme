366900b00d8cd4189d7eaaf115a93c9d9e93f023
enessancar <enes57751907@gmail.com>
2024-02-06 22:28:28 +0300

    Initial Commit

---
+++ Proje/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proje
+//
+//  Created by Enes Sancar on 6.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
